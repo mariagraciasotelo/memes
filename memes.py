6d228c3a6ad6cdb750000ae3d312b684cce203a8
mariagraciasotelo <161959902+mariagraciasotelo@users.noreply.github.com>
2024-04-13 11:24:39 -0500

    Create memes.py

---
+++ memes.py
@@ -0,0 +1,47 @@
+import os
+import random
+import requests
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def mem(ctx):  
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def mem_animals(ctx):
+   animals = random. choice(os.listdir('animals_memes'))
+with open(f' animals_memes/{animals}', 'rb') as f:
+     a_picture = discord. File(f)
+await ctx.send(file=a_picture)
+
+bot.run("token")
